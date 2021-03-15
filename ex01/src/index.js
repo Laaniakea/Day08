1d2742087f4bdeca57549e99295f7f557b376c6e
unknown <edina.kurtic90@hotmail.com>
2021-03-15 21:28:38 +0100

    ex01 was added, my fist FUnction

---
+++ ex01/src/index.js
@@ -0,0 +1,14 @@
+
+// Write code below this line
+
+function reusableFunction() {
+    console.log("Hi Padawan!");
+  
+}
+reusableFunction ();
+
+// Wrate code above this line
+
+
+module.exports = reusableFunction;
+
