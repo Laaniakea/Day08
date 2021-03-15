fa91a48c5a73fc5c8f76c6459e9e580c08b4d879
unknown <edina.kurtic90@hotmail.com>
2021-03-15 22:00:10 +0100

    ex02 functions with arguments was edites

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,11 +1,11 @@
 // Write code below this line
 function functionPrintInfo(name, age) {
-console.log("John", 23);
-console.log("Michael", 43);
+    console.log("John", 23);
+    console.log("Michael", 43);
 
 }
 functionPrintInfo();
 
-// Write code abode this line
+// Write code above this line
 
 module.exports = functionPrintInfo;
