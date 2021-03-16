22e625196ec0e0a060de227c38f0f7ee4347987c
unknown <edina.kurtic90@hotmail.com>
2021-03-16 10:52:09 +0100

    ex12 was added

---
+++ ex12/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+function randomInteger() {
+
+ Math.floor(Math.random() * 30);
+
+}
+randomInteger();
+
+// Only change code above this line
+module.exports = randomInteger;
