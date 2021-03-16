5add4a8ed08910abf93ce57878033b80a3fc3d19
unknown <edina.kurtic90@hotmail.com>
2021-03-16 10:53:26 +0100

    ex13 was added

---
+++ ex13/src/index.js
@@ -0,0 +1,13 @@
+
+// Only change code below this line
+
+
+function randomRangeNumber(minNumber, maxNumber) {
+
+    return Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+}
+
+console.log(randomRangeNumber(20, 40));
+// Only change code above this line
+
+module.exports = randomRangeNumber;
